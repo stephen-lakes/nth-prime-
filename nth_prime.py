460a03e99987e42936fafb2dcbad444757cbccbb
stephen <52784102+stephen-lakes@users.noreply.github.com>
2021-01-02 12:53:38 +0100

    Add files via upload

---
+++ nth_prime.py
@@ -0,0 +1,23 @@
+def nth_prime(n:int):
+    primes:list = []
+    counter:int = 2
+
+    while len(primes) < n:
+
+        if counter == 2 or counter == 3:
+            primes.append(counter)
+        else:
+            for num in range(2, (counter // 2) + 1):
+                if counter % num == 0:
+                    break
+            else:
+                primes.append(counter)
+        
+        counter += 1
+
+    return primes[-1]
+
+
+
+# print the 100001th prime number
+print(nth_prime(100001))
