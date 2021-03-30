157db9515c47125e79f10f61838b9c2d2d86e7a8
stephen <oluyomiolamilekan99@gmail.com>
2021-03-31 00:32:20 +0100

    Update nth_prime.py

--- nth_prime.py
+++ nth_prime.py
@@ -1,23 +1,33 @@
-def nth_prime(n:int):
-    primes:list = []
-    counter:int = 2
-
-    while len(primes) < n:
-
-        if counter == 2 or counter == 3:
-            primes.append(counter)
+def is_prime(n:int) -> bool:
+    """
+    check if a number is prime.
+    """
+    if n == 0 or n == 1:
+        return False
+    elif n == 2 or n == 3:
+        return True
+    else:
+        for num in range(2, (n//2) + 1):
+            if n % num == 0:
+                return False
         else:
-            for num in range(2, (counter // 2) + 1):
-                if counter % num == 0:
-                    break
-            else:
-                primes.append(counter)
-        
-        counter += 1
-
-    return primes[-1]
+            return True
 
 
+def nth_prime(n:int) -> int:
+    """
+    returns the nth prime.
+    """
+    prime:int = 0
+    count:int = 0
 
-# returns the 100001th prime number
-print(nth_prime(100001))
+    while count < n:
+        prime = prime + 1
+        if is_prime(prime):
+            count = count + 1
+
+    return prime
+
+
+# prints the 100001th prime number
+print(nth_prime(10001)) 
